2cc85cd6a21a27f1d173cc388c0301f41ac5b776
Tu Nombre <tu@email.com>
2023-10-31 00:42:48 -0600

    testbench

--- 3_project/UVM_example/testbench.sv
+++ 3_project/UVM_example/testbench.sv
@@ -1,19 +1,16 @@
-module tb_top;
-  import uvm_pkg::*;  //Se importa UVM pckg_sz para usar construcciones de UVM en el modulo 
-  
-  bit clk;
-  always #10 clk <= ~clk;
-  
+module tb;  
+  reg clk;
 
-// La interfaz se establece como un objeto en uvm_config_db (base de datos de UVM) y se recupera en la fase de prueba 
-//  usando el método run_test();
+  always #10 clk =~ clk;
 
-  dut_if dut_if1(clk);
-  dut_wrapper dut_wr0 (._if(dut_if1)) // Se coloca el dispositivo bajo prueba dentro de wrapper
+  des_if _if(clk);
+
+  det_1011 u0 (.clk(clk), .rstn(_if.rstn), .in(_if.in), .out(_if.out));
   
   initial begin
-    uvm_config_db #(virtual dut_if)::set(null, "uvm_test_top", "dut_if", dut_if1);
-    run_test ("base_test"); // Se corre la prueba base_test en la simulacion
+    clk <= 0;
+    uvm_config_db #(virtual des_if)::set(null, "uvm_test_top", "des_if", _if);
+    run_test ("test_1011"); 
   end
   
   initial begin
