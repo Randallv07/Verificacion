7076a00a1df74200acd32ca1b348de3f0995c132
Tu Nombre <tu@email.com>
2023-10-30 23:45:26 -0600

    sequence

--- 3_project/UVM_example/sequence.sv
+++ 3_project/UVM_example/sequence.sv
@@ -1,21 +1,35 @@
-class my_sequence extends uvm_sequence;
-    `uvm_object_utils (my_sequence)
+class gen_item_seq extends uvm_sequence;
 
-    function new (string name = "my_sequence");
-        super.new (name);
+    `uvm_object_utils (gen_item_seq)
+
+    function new(string name="gen_item_seq");
+
+        super.new(name);
+
     endfunction
 
-    task pre_body ();
+    rand int num; 
+
+    constraint c1 { soft num inside ([10:50]); }
+
+    virtual task body();
+
+        for (int i = 0; i < num; i++) begin
+
+        Item m_item Item::type_id::create("w_item");
+
+        start_item(m_item);
+
+        m_item.randomize();
+
+        uvm_info("SEQ", $sformatf("Generate new item: %s", m_item.convert2str()), UVM_HIGH)
+
+        finish item(m_item);
+
+        end
+
+        `uvm_info("SEQ", $sformatf("Done generation of %0d items", num), UVM_LOW)
 
     endtask
 
-    task body ();
-    my_data pkt;
-    `uvm_do (pkt);
-    endtask
-
-    task post_body();
-
-    endtask
-    
 endclass
