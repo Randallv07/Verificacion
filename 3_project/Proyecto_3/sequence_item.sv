5ddd937f278066ffab2f9e2a418f4fe514f157bf
Randallv07 <randallv07@gmail.com>
2023-11-07 15:18:41 -0600

    Sequence item

--- 3_project/Proyecto_3/sequence_item.sv
+++ 3_project/Proyecto_3/sequence_item.sv
@@ -38,35 +38,50 @@ class item extends uvm_sequence_item;
     bit  [3:0] f_fila;  // Fila de fuente
     bit  [3:0] f_columna;  // Columna de fuente
 
-    virtual function string print_item_in(); // Prints the input item
-        return $sformatf("fp_X=%0h, fp_Y=%0h, r_mode=%0d",
-        fp_X,fp_Y,r_mode);
+    //Otros
+    int tiempo;
+    int variabilidad;
+    rand int retardo;
+    int retardo_max;
+    int fuente_aux;
+
+
+    virtual function string print_transaccion(); // Imprime la transacción
+        return $sformatf("Fila_d=%0h, Columna_d=%0h, Modo=%0d, Fila_f=%0d, Columna_F=%0d, Pyld=%0d",
+        d_fila, d_columna, modo, f_fila, f_columna, dato);
     endfunction
 
-    virtual function string print_item_out(); // Prints the output item
-        return $sformatf("fp_X=%0h, fp_Y=%0h, fp_Z=%0h, r_mode=%0d, ovrf=%0b, udrf=%0b",
-        fp_X,fp_Y,fp_Z,r_mode,ovrf,udrf);
-    endfunction
 
     // *************************************Constraints*************************************
 
-    // Rounding mode	
-    constraint c_rounding {r_mode inside{3'b000,3'b001,3'b010,3'b011,3'b100};}
+    //Modo
+    constraint MODO_1 {modo == 1;};
+    constraint MODO_0 {modo == 0;};
+    //Fuente
+    constraint Existe_fuente {fuente >= 0; fuente < 2*ROWS+2*COLUMS;}; 
+    constraint send_self {c_fila != pos_driver[fuente].fila; c_columna != pos_driver[fuente].colum;};
+    //Dirección
+    constraint Existe_direccion {c_fila <= ROWS+1; c_fila >= 0; c_columna <= COLUMS+1; c_columna >= 0;};
+    constraint restric_columna {
+    if(c_fila == 0 | c_fila == ROWS+1) 
+        c_columna <= COLUMS & c_columna > 0;
+    };
+    constraint restric_fila {
+    if(c_columna == 0 | c_columna == COLUMS+1) 
+        c_fila <= ROWS & c_fila > 0;
+    };
+    constraint direccion_valida_drvs {
+    if(c_fila != 0 & c_fila != ROWS+1)
+        c_columna == 0 | c_columna == COLUMS+1;
+    };
+    //Datos
+    constraint variabilidad_dato {dato inside {{(pckg_sz-25){2'b10}},{(pckg_sz-25){2'b01}},{(pckg_sz-25){1'b1}},{(pckg_sz-25){1'b0}}};};// Variabilidad maxima
 
-    // Specific values
-    constraint c_specific {
-    fp_X inside {32'h00000000,32'h80000000,32'hFFFFFFFF,32'h7FFFFFFF,32'h7FC00000,32'hFFC00000,32'h55555555,32'hAAAAAAAA};
-    fp_Y inside {32'h00000000,32'h80000000,32'hFFFFFFFF,32'h7FFFFFFF,32'h7FC00000,32'hFFC00000,32'h55555555,32'hAAAAAAAA};}
+    //Fuente estatica
+    constraint Fuente_estatica {fuente == fuente_aux;};
 
-    // Constraint for overflow
-    constraint c_overflow {fp_X[30:23] + fp_Y[30:23]  >= 382;}
+    //Retardo
+    constraint retardo_aleat{retardo<=retardo_max;retardo>0;};
+    constraint retardo_0{retardo == 0;};
 
-    // Constraint for  underflow
-    constraint c_underflow {fp_X[30:23]  + fp_Y[30:23] <= 126;}
-
-    // Constraint for NaN
-    constraint c_nan {(&fp_X[30:22])&(~|fp_X[21:0]) | (&fp_Y[30:22])&(~|fp_Y[21:0]);}
-
-    // Constraint between overflow and underflow 
-    constraint c_between {fp_X[30:23] + fp_Y[30:23] > 126; fp_X[30:23] + fp_Y[30:23] < 382;}
 endclass
