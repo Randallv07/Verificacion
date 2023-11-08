cef4157cc9d3fb571980fa2495c9916bcfae94de
Emanuel <emanuelarayaesq@estudiantec.cr>
2023-11-07 23:49:55 -0600

    Driver parcialmente, Agente completo y fifo in

--- 3_project/Proyecto_3/Driver.sv
+++ 3_project/Proyecto_3/Driver.sv
@@ -1,4 +1,4 @@
-class driver extends uvm_driver #(Item);
+class driver extends uvm_driver #(item);
 `uvm_component_utils(driver)
 
 //Constructor 
@@ -6,10 +6,10 @@ function new(string name = "driver", uvm_phase parent = null);
     super.new(name,parent);
 endfunction 
 
+
 //Llamando a la interfaz de del DUT
 
 virtual bus_mesh_if  vif;
-
 
 virtual function void  build_phase(uvm_phase phase); //Se conecta el bus_mesh_if con vif para la interfaz virtual.
     super.build_phase(phase);
@@ -20,17 +20,39 @@ endfunction
 
 virtual task run_phase(uvm_phase phase);
     super.run_phase(phase);
-forever begin
 
-    Item s_item;
+//Declarando la instancia de la FIFO
+    fifo_in fifo_in_inst;
+
+forever begin
+    item s_item;
     `uvm_info("Driver", $formatf("Esperando por item de secuencia"),UVM_HIGH)
     seq_item_port.get_next_item(s_item);
-    driver_item(s_item)
-    seq_item_port.item_done(s_item)
+    driver_item(s_item);
+    seq_item_port.item_done(s_item);
+
+    //Realizar operaciones con la FIFO
+    fifo_inst.Fin_push();
+
+    fork
+        fifo_inst.interfaz();
+    join_none
+
+
+
+   /* virtual task driver_item(s_item)
+    @(posedge vif.clk);
+    begin
+
+        vif.pndng <= 
+        vif.data_out <=
+        vif.popin <=
+        vif.pop <=
+        vif.data_out_i_in <=
+        vif.pndng_i_in <=
+        vif.reset <= */ 
 
 end
-
-
 endtask 
 
 endclass
--- 3_project/Proyecto_3/agente.sv
+++ 3_project/Proyecto_3/agente.sv
@@ -1,23 +1,39 @@
 class agent extends uvm_agent;
 	`uvm_component_utils(agent); // Register at the factory
+	parameter int  ROWS = 4;
+	parameter int  COLUMS = 4;
+	parameter int  pckg_sz = 40;
+	parameter int  fifo_depth = 4;
+	parameter int drvrs = [ROWS*2 + COLUMS*2];
 
 	function new(string name = "agent", uvm_component parent=null); // Builder
 		super.new(name,parent);
 	endfunction
 
-	uvm_sequencer #(item) 	seq_inst;	// Sequencer instance
-	driver	      	 	drv_inst;	// Driver instance
-	monitor 	        mnt_inst;	// Monitor instance
+	driver	      	 	    drv_inst;	// Instancia del driver
+	monitor 	            mnt_inst;	// Instancia del monitor
+	uvm_sequencer #(item)   sequencer_inst //Instancia del sequencer
 	
 	virtual function void build_phase(uvm_phase phase);
 		super.build_phase(phase);
-		seq_inst = uvm_sequencer#(item)::type_id::create("seq_inst",this);
-		drv_inst = driver::type_id::create("drv_inst",this);
-		mnt_inst = monitor::type_id::create("mnt_inst",this);
+		
+		// Generando instancias de los drivers
+		for (int i = 0; i < drvrs ; i++) begin
+    		string name = $sformatf("drv%d", i);
+    		driver drv_inst = driver::type_id::create(name, null);
+		end
+
+		//Generando instancias de los monitores
+		for (int j = 0; j < drvrs; j++) begin
+			string name2 = $sformatf("mnt%d", j);
+    		monitor mnt_inst = monitor::type_id::create(name2, null);
+		end
+
+
 	endfunction
 	
 	virtual function void connect_phase(uvm_phase phase);
 		super.connect_phase(phase);
-		drv_inst.seq_item_port.connect(seq_inst.seq_item_export);
+		drv_inst.seq_item_port.connect(sequencer_inst.seq_item_export);
 	endfunction
 endclass
